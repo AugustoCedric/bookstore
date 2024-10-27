bfa0791eeabc6cab1db51dd41518fe386403bf7b
AugustoCedric <cedric.pmi.rmp@gmail.com>
2024-10-27 08:57:12 -0300

    reconfiguração geral

--- product/tests/test_viewsets/test_category_viewset.py
+++ product/tests/test_viewsets/test_category_viewset.py
@@ -7,6 +7,7 @@ from rest_framework.views import status
 from product.factories import CategoryFactory
 from product.models import Category
 
+
 class CategoryViewSet(APITestCase):
     client = APIClient()
 
@@ -17,11 +18,12 @@ class CategoryViewSet(APITestCase):
         response = self.client.get(
             reverse('category-list', kwargs={'version': 'v1'})
         )
-=
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         category_data = json.loads(response.content)
 
         self.assertEqual(category_data["results"][0]['title'], self.category.title)
+
 
     def test_create_category(self):
         data = json.dumps({
